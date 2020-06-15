cfba0e7048c879ced7f980af8ac12fb8c6648d30
pedromirandarf <pedro.celsomrf@gmail.com>
2020-06-15 20:00:38 -0300

    Testando knex prodution v3

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING1,
+    connection: process.env.DATABASE_URL,
     searchPath: ['knex', 'public'],
     migrations: {
       tableName: 'knex_migrations',
