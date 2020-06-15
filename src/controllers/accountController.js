74981b50176e63c7182ca66c10f1f28c73364070
pedromirandarf <pedro.celsomrf@gmail.com>
2020-06-15 15:27:42 -0300

    Teste heroku

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const req = require('request');
+const request = require('request-promise');
+require('dotenv').config()
+const Chave = process.env.ACKEY;
+
+let colect_data = async (req,res)=>{
+    let account_data = [];
+
+    console.log("Inciando coleta de dados");
+    var endpoint = "/api/3/accounts?limit=100&offset=100";
+    
+    var allData = await request({
+        method: 'GET',
+        url: `https://compugraf55051.api-us1.com${endpoint}`,
+        headers:{
+            "Api-Token": Chave
+        },
+        json: true
+    });
+    //const dataSize = allData["meta"]["total"];
+    
+    
+    //console.log(allData);
+    account_data = allData;
+
+    return account_data;
+};
+
+
+
+
+
+
+
+
+
 
 router.get('/account', async(req,res)=>{
     try{
-        return res.send("Top demais rogerinho");
+        const accounts = await colect_data();
+        return res.send({
+            message: "Tudo certo",
+            Data: accounts
+        });
     }catch(err){
         return res.status(400).send({error: err})
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors =require('cors');
-
+require('dotenv').config()
 
 const app = express();
 
@@ -10,4 +10,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 require('./controllers/accountController')(app);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
